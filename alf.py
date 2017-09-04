f1f343d2099fd59f1b73c98fb2e5e145e40c4d46
Harald Kube <harald.kube@gmx.de>
2017-09-04 21:11:44 +0200

    Final version of the processing pipeline

--- alf.py
+++ alf.py
@@ -27,28 +27,28 @@ def calcCalibrationMatrix(imgNameList, numOfCorners):
     imgpoints = []
     grayShape = None
 
+    # Iterate through the list of calibration images
     for imgName in imgNameList:
+        # Open the image file
         img = mpimg.imread(imgName)
         if len(img.shape) > 2:
             # Convert image to grayscale
             gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         else:
+            # The image is already a grayscale image
             gray = img
 
+        # Find chessboard corners
         ret, corners = cv2.findChessboardCorners(gray, numOfCorners, None)
         if ret:
+            # Chessboard corners were found
             print("Use image", imgName)
             grayShape = gray.shape
-            #print("Corners found:", corners)
             objpoints.append(objpTemp)
-            if False:
-                corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-                imgpoints.append(corners2)
-            else:
-                imgpoints.append(corners)
-#            img2 = cv2.drawChessboardCorners(img, numOfCorners, corners2, ret)
-#            plt.imshow(img2)
+            corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners)
         else:
+            # Chessboard corners weren't found
             print("Ignore image", imgName)
 
     ret = False
@@ -58,6 +58,7 @@ def calcCalibrationMatrix(imgNameList, numOfCorners):
     tvecs = None
 
     if grayShape is not None:
+        # At least one calibration image can be used
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, grayShape[::-1], None, None)
 
     return ret, mtx, dist, rvecs, tvecs
@@ -68,6 +69,7 @@ def getCameraCalibrationMatrix(listOfCalImgs, calDataFile, forceCalculation):
         # Calculate camera calibration data
         cv2ret, mtx, dist, rvecs, tvecs = calcCalibrationMatrix(listOfCalImgs, (9,6))
 
+        # Save camera calibration data
         dist_pickle = {}
         dist_pickle["mtx"] = mtx
         dist_pickle["dist"] = dist
@@ -82,11 +84,270 @@ def getCameraCalibrationMatrix(listOfCalImgs, calDataFile, forceCalculation):
 
     return mtx, dist
 
+####################
+def undistortImage(img, mtx, dist, useOptimalMatrix=False):
+    h, w = img.shape[:2]
+    if useOptimalMatrix:
+        newCamMtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 0, (w,h))
+        imgUndist = cv2.undistort(img, mtx, dist, None, newCamMtx)
+    else:
+        imgUndist = cv2.undistort(img, mtx, dist, None, mtx)
+    return imgUndist
+
+####################
+def searchLanes(img, numOfHistWindows):
+    # Calculate the histogramm of the lower half of the image
+    histogram = np.sum(img[img.shape[0]//2:,:], axis=0)
+
+    # Smooth the histogram
+    smoothWin = np.ones(51)
+    histogram = np.convolve(smoothWin/smoothWin.sum(), histogram, 'same')
+
+    midpoint = histogram.shape[0]//2
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    print("left_base:", leftx_base, "  right_base:", rightx_base)
+    window_height = img.shape[0]//numOfHistWindows
+    nonzero = img.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    margin = 100
+    minpix = 50
+    left_lane_inds = []
+    right_lane_inds = []
+
+    for window in range(numOfHistWindows):
+        # Calculate the scan windows borders
+        win_y_bottom = img.shape[0] - (window+1)*window_height
+        win_y_top = img.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_bottom) & (nonzeroy < win_y_top) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_bottom) & (nonzeroy < win_y_top) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    return leftx, lefty, rightx, righty, nonzerox, nonzeroy, left_lane_inds, right_lane_inds
+
+
+
+
+
+
+
+
+
+####################
 if __name__ == '__main__':
     FORCE_CALIBRATION = False
     CALIB_DATA_FILE_NAME = "./cam_calib_data.p"
+    CROP_Y = 453
+    WARP_X_BOTTOM   = 200
+    WARP_X_TOP_LEFT = 590
+    SOBEL_MAG_THRESH = (10, 250)
+    NUM_OF_HIST_WINDOWS = 9
+
+    ym_per_pixel = 30/(720-CROP_Y)
+    xm_per_pixel = 3.7/700
+
+    # Select the video: "project", "challenge" or "harder_challenge"
+    videoName = "project"
+
 
     # Load or calculate camera calibration matrix
     fileList = glob.glob("./camera_cal/calibration*.jpg")
     mtx, dist = getCameraCalibrationMatrix(fileList, CALIB_DATA_FILE_NAME, FORCE_CALIBRATION)
-    pass
+
+    #imgName = "test_images/straight_lines1.jpg"
+#    imgName = "test_images/test3.jpg"
+#    img = mpimg.imread(imgName)
+
+    videoFileName = "{:s}_video.mp4".format(videoName)
+
+    imgName = videoFileName
+    videoReader = cv2.VideoCapture(videoFileName)
+    videoWriter = cv2.VideoWriter("./output_images/{:s}_video.mp4".format(videoName), cv2.VideoWriter_fourcc(*'mp4v') , 25.0, (1280,720))
+    if not videoWriter.isOpened():
+        print("ERROR: Could not open VideoWriter")
+        exit()
+
+    frameNo = 0
+    while videoReader.isOpened():
+        ret, img = videoReader.read()
+
+        if not ret:
+            break;
+        frameNo += 1
+
+        if videoName == "project" and True:
+            framesToSave = [559, 1039, 1040, 1151]
+            if frameNo < 1030:
+                if (frameNo % 100) == 0:
+                    print("Skipping {:d}".format(frameNo))
+                continue
+                pass
+            elif frameNo > 1050:
+                break
+                pass
+            elif frameNo in framesToSave:
+                cv2.imwrite("./test/{:s}_video_{:d}.jpg".format(videoName, frameNo), img)
+        elif videoName == 'challenge' and True:
+            framesToSave = [1, 79, 104, 127, 130, 131, 133]
+            if frameNo < 95:
+                if (frameNo % 100) == 0:
+                    print("Skipping {:d}".format(frameNo))
+                continue
+                pass
+            elif frameNo > 165:
+                break
+                pass
+            elif frameNo in framesToSave:
+                cv2.imwrite("./test/{:s}_video_{:d}.jpg".format(videoName, frameNo), img)
+
+#        print("***** frame:", frameNo)
+        imgUndist = undistortImage(img, mtx, dist)
+        h, w = imgUndist.shape[:2]
+#        print("imgUndist.shape:", imgUndist.shape)
+        # Crop the upper part of the image
+        imgCrop = imgUndist[CROP_Y:h, :]
+        # Warp image
+        warpXTopRight = imgCrop.shape[1]-WARP_X_TOP_LEFT
+        hCrop = imgCrop.shape[0]
+        srcRect2 = ((WARP_X_BOTTOM,hCrop), (w-WARP_X_BOTTOM,hCrop), (warpXTopRight, 0), (WARP_X_TOP_LEFT, 0))
+        dstRect  = ((WARP_X_BOTTOM,h), (w-WARP_X_BOTTOM,h), (w-WARP_X_BOTTOM, 0), (WARP_X_BOTTOM, 0))
+#        print("srcRect:", srcRect2)
+#        print("dstRect:", dstRect)
+
+        Mwarp   = cv2.getPerspectiveTransform(np.float32(srcRect2), np.float32(dstRect))
+        Munwarp = cv2.getPerspectiveTransform(np.float32(dstRect),  np.float32(srcRect2))
+
+        imgWarped = cv2.warpPerspective(imgCrop, Mwarp, (w,h), flags=cv2.INTER_LINEAR)
+#        imgHlsW = cv2.cvtColor(imgWarped, cv2.COLOR_RGB2HLS)
+        if False:
+            imgHlsW = cv2.cvtColor(imgWarped, cv2.COLOR_BGR2HLS)
+            imgSat = imgHlsW[:,:,2]
+        else:
+            ## Convert to color space CMY (cyan plane is sufficient)
+            #imgSat = np.zeros(imgWarped.shape[:2])
+            #imgSat[:,:] = 255-imgWarped[:,:,2]
+
+            # Use the bright parts of the red plane
+            imgSat = np.copy(imgWarped[:,:,2])
+            lowLimit = int(imgSat.mean() * 1.3)
+            np.putmask(imgSat, imgSat<lowLimit, lowLimit)
+
+        imgSobelAbs = np.abs(cv2.Sobel(imgSat, cv2.CV_64F, 1, 0))
+        imgSobelScale = np.uint8(255.0*imgSobelAbs/imgSobelAbs.max())
+        imgSobelBin = np.zeros_like(imgSat)
+        imgSobelBin[(SOBEL_MAG_THRESH[0] < imgSobelScale) & (imgSobelScale < SOBEL_MAG_THRESH[1])] = 1
+
+        leftx, lefty, rightx, righty, nonzerox, nonzeroy, left_lane_inds, right_lane_inds = searchLanes(imgSobelBin, NUM_OF_HIST_WINDOWS)
+
+#        print("len(leftx) :", len(leftx))
+#        print("len(rightx):", len(rightx))
+
+        # Fit a second order polynomial to each
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+
+#        print("left_fit:", left_fit, "   right_fit:", right_fit)
+
+        left_fit_cr = np.polyfit(lefty * ym_per_pixel, leftx * xm_per_pixel, 2)
+        right_fit_cr = np.polyfit(righty * ym_per_pixel, rightx * xm_per_pixel, 2)
+
+        # Calculate curvature
+        ploty = np.linspace(0, imgSobelBin.shape[0]-1, imgSobelBin.shape[0] )
+        y_eval = np.max(ploty)
+
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+
+
+        left_curverad  = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pixel + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+        right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pixel + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+
+#        print("left radius:", left_curverad, "m,  right radius:", right_curverad, "m")
+
+
+        lane_pos_y = imgSobelBin.shape[0]-1
+        left_lane_pos = left_fit[0]*lane_pos_y**2 + left_fit[1]*lane_pos_y + left_fit[2]
+        right_lane_pos = right_fit[0]*lane_pos_y**2 + right_fit[1]*lane_pos_y + right_fit[2]
+
+        #print("lane pos left:", left_lane_pos, "  right:", right_lane_pos)
+        #print("lane pos type(left):", type(left_lane_pos), "  right:", right_lane_pos)
+
+        if len(leftx) > len(rightx):
+            curveRadius = left_curverad
+#            print("left_fit :", left_fit_cr)
+        else:
+            curveRadius = right_curverad
+#            print("right_fit:", right_fit_cr)
+
+        lane_center = (left_lane_pos + right_lane_pos) / 2
+        dist_from_lane_center = lane_center - imgSobelBin.shape[1]//2
+        abs_dist_in_meters = np.abs(dist_from_lane_center * xm_per_pixel)
+        if dist_from_lane_center < 0:
+            position = "right"
+        else:
+            position = "left"
+
+        print("Frame: {:d}  radius: {:8.0f}  position: {:5.2f} {:s} of center".format(frameNo, curveRadius, abs_dist_in_meters, position))
+
+        textRadius   = "Radius of curvature: {:8.0f}m".format(curveRadius)
+        textPosition = "Vehicle is {:5.2f}m {:s} of center".format(abs_dist_in_meters, position)
+#        print(textRadius)
+#        print(textPosition)
+
+        overlayOffs = imgUndist.shape[0] - imgCrop.shape[0]
+
+        imgMarked = np.zeros_like(imgUndist)
+        imgMarked[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        imgMarked[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 255, 255]
+        for i in range(len(left_fitx)):
+            y = int(ploty[i])
+        #    cv2.circle(imgMarked, (int(left_fitx[i]), y), 5, (255,0,0))
+        #    cv2.circle(imgMarked, (int(right_fitx[i]), y), 5, (0, 255,255))
+            cv2.line(imgMarked, (int(left_fitx[i]), y), (int(right_fitx[i]), y), color=(0, 255,0))
+
+        imgUnwarped = np.zeros_like(imgMarked)
+        imgUnwarped[:,:] = cv2.warpPerspective(imgMarked, Munwarp, (w, h), flags=cv2.INTER_LINEAR)
+
+        imgOverlayed = np.copy(imgUndist)
+
+        imgUnwarpedShifted = np.zeros_like(imgUnwarped)
+        imgUnwarpedShifted[overlayOffs:,:] = imgUnwarped[:imgCrop.shape[0],:]
+        imgOverlayed = cv2.addWeighted(imgUndist, 1, imgUnwarpedShifted, 0.2, 0)
+        cv2.putText(imgOverlayed, textRadius,   (150, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255))
+        cv2.putText(imgOverlayed, textPosition, (150,100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255))
+        cv2.putText(imgOverlayed, "Frame: {:d}".format(frameNo), (150,150), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255))
+
+        #mpimg.imsave("./output_images/"+os.path.basename(imgName), imgOverlayed)
+        videoWriter.write(imgOverlayed)
+
+    videoWriter.release()
+    videoReader.release()
+
